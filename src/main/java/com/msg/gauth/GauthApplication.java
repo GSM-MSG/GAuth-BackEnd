8316031f8c8e6c78ad268a91f9d71723042da9f9
jeayoung <k01066624566@gmail.com>
2022-07-29 14:57:22 +0900

    init setting

---
+++ src/main/java/com/msg/gauth/GauthApplication.java
@@ -0,0 +1,13 @@
+package com.msg.gauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GauthApplication.class, args);
+	}
+
+}
