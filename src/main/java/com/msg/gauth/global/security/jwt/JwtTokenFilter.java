b66379619b6e9bd92b4a2112f9b975c5a7af2f25
baegteun <baegteun@gmail.com>
2022-07-30 22:20:48 +0900

    :sparkles: :: JwtTokenFilter

---
+++ src/main/java/com/msg/gauth/global/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,31 @@
+package com.msg.gauth.global.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String bearer = jwtTokenProvider.resolveToken(request);
+        if (bearer != null) {
+            Authentication authentication = jwtTokenProvider.authentication(bearer);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
