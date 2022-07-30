3935378e2b81a4b2fda3af1e185f395e75dcd51b
baegteun <baegteun@gmail.com>
2022-07-30 20:40:07 +0900

    :sparkles: :: AuthDetailsService

---
+++ src/main/java/com/msg/gauth/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.msg.gauth.domain.user.repository;
+
+import com.msg.gauth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/msg/gauth/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,24 @@
+package com.msg.gauth.global.security.auth;
+
+import com.msg.gauth.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(AuthDetails::new)
+                .orElseThrow(); // TODO: throw User Not Found Exception
+    }
+}
