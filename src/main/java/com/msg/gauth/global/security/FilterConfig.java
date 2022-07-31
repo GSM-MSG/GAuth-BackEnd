82d6c6769453a43a91e5f3ca00f15b9ca3dfd0d1
baegteun <baegteun@gmail.com>
2022-07-31 16:24:49 +0900

    :sparkles: :: FilterConfig

---
+++ src/main/java/com/msg/gauth/global/security/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.msg.gauth.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msg.gauth.global.security.filter.ExceptionFilter;
+import com.msg.gauth.global.security.filter.JwtTokenFilter;
+import com.msg.gauth.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void configure(HttpSecurity builder) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        ExceptionFilter exceptionFilter = new ExceptionFilter(objectMapper);
+
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(exceptionFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
