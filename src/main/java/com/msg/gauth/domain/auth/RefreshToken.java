fa78f435eb1badfd263d5fc4df38e0d65344335e
baegteun <baegteun@gmail.com>
2022-08-01 16:51:21 +0900

    :pencil2: :: 사소한 실수~

--- src/main/java/com/msg/gauth/domain/auth/RefreshToken.java
+++ src/main/java/com/msg/gauth/domain/auth/RefreshToken.java
@@ -1,11 +1,11 @@
 package com.msg.gauth.domain.auth;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 
 @RedisHash
 @AllArgsConstructor
