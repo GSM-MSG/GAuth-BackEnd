01130b39386b732ce4ea12ed2532114e35698837
jeayoung <k01066624566@gmail.com>
2022-08-01 18:06:00 +0900

    피드백 반영

--- src/test/java/com/msg/gauth/user/signup/SignUpServiceTest.java
+++ src/test/java/com/msg/gauth/user/signup/SignUpServiceTest.java
@@ -5,10 +5,19 @@ import com.msg.gauth.domain.user.exception.UserNotFoundException;
 import com.msg.gauth.domain.user.presentation.dto.request.SignUpDto;
 import com.msg.gauth.domain.user.repository.UserRepository;
 import com.msg.gauth.domain.user.services.SignUpService;
+import com.msg.gauth.global.exception.exceptions.DuplicateEmailException;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
 
 @SpringBootTest
 public class SignUpServiceTest {
@@ -17,11 +26,19 @@ public class SignUpServiceTest {
     private SignUpService signUpService;
     @Autowired
     private UserRepository userRepository;
+    private static ValidatorFactory validatorFactory;
+    private static Validator validatorFromFactory;
+
+    @BeforeAll
+    public static void init() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validatorFromFactory = validatorFactory.getValidator();
+    }
 
     @Test
     public void SignUp(){
         //given
-        SignUpDto signUpDto = new SignUpDto("s21053@gsm.hs.kr", "1234");
+        SignUpDto signUpDto = new SignUpDto("s21053@gsm.hs.kr", "123456789999");
 
         //when
         Long userId = signUpService.execute(signUpDto);
@@ -31,5 +48,20 @@ public class SignUpServiceTest {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException());
         Assertions.assertThat(user.getEmail()).isEqualTo(signUpDto.getEmail());
+        org.junit.jupiter.api.Assertions.assertThrows(DuplicateEmailException.class, () -> signUpService.execute(signUpDto));
+    }
+
+    @Test
+    public void PasswordValidationTest(){
+        SignUpDto signUpDto = new SignUpDto("s21053@gsm.hs.kr", "123");
+        Set<ConstraintViolation<SignUpDto>> validate = validatorFromFactory.validate(signUpDto);
+        Assertions.assertThat(validate).isNotEmpty();
+    }
+
+    @Test
+    public void EmailValidationTest(){
+        SignUpDto signUpDto = new SignUpDto("s21053hs.kr", "123219081");
+        Set<ConstraintViolation<SignUpDto>> validate = validatorFromFactory.validate(signUpDto);
+        Assertions.assertThat(validate).isNotEmpty();
     }
 }
