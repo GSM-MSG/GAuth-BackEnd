9ea8985ae9219a368799079054353ba76357d399
baegteun <baegteun@gmail.com>
2022-09-21 17:42:56 +0900

    Cors

---
+++ src/main/kotlin/com/msg/gauth/global/web/WebMvcConfig.kt
@@ -0,0 +1,15 @@
+package com.msg.gauth.global.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
+}
