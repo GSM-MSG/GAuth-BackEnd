51c96b0ddcb0431f8cf0960d7e93807f99bc30ff
baegteun <baegteun@gmail.com>
2022-09-21 09:36:33 +0900

    build :: Kotlin version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+    kotlin("plugin.jpa") version "1.7.10"
+    application
 }
 
 group = "com.msg"
@@ -49,4 +50,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
 }
