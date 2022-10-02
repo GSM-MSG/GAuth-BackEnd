53def17feaaa696934bce4ef7563cb1c5e5ae4b2
jeayoung <k01066624566@gmail.com>
2022-10-02 15:38:03 +0900

    ClientDto 이름 변경

--- src/main/kotlin/com/msg/gauth/domain/client/persentation/dto/response/ClientDetailResDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/client/persentation/dto/response/ClientDetailResDto.kt
@@ -2,13 +2,17 @@ package com.msg.gauth.domain.client.persentation.dto.response
 
 import com.msg.gauth.domain.client.Client
 
-data class ClientAllResDto(
+data class ClientDetailResDto(
     val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String,
     val serviceName: String,
     val serviceUri: String,
-) {
+){
     constructor(client: Client): this(
         clientId = client.clientId,
+        clientSecret = client.clientSecret,
+        redirectUri = client.redirectUri,
         serviceName = client.serviceName,
         serviceUri = client.serviceUri,
     )
