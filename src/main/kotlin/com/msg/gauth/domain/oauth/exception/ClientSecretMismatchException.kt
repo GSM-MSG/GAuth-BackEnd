3a1965e814bbb618ed19ff8ed1a2f2e10c0d6ab7
조재영 <80161826+dolong2@users.noreply.github.com>
2022-10-12 16:37:48 +0900

    Update src/main/kotlin/com/msg/gauth/domain/oauth/exception/ClientSecretMismatchException.kt
    Co-authored-by: baegteun <74440939+baekteun@users.noreply.github.com>

--- src/main/kotlin/com/msg/gauth/domain/oauth/exception/ClientSecretMismatchException.kt
+++ src/main/kotlin/com/msg/gauth/domain/oauth/exception/ClientSecretMismatchException.kt
@@ -3,4 +3,4 @@ package com.msg.gauth.domain.oauth.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class ClientSecretMismatchException:  BasicException(ErrorCode.CLIENT_SECRET_MISMATCH)
+class ClientSecretMismatchException: BasicException(ErrorCode.CLIENT_SECRET_MISMATCH)
