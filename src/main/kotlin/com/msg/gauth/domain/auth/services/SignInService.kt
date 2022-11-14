75cabb3f8490b8a0b7c6f8d2a80e3965ce8ed4f1
jeayoung <k01066624566@gmail.com>
2022-11-15 08:17:25 +0900

    UserState가 created일때 로그인되게 수정

--- src/main/kotlin/com/msg/gauth/domain/auth/services/SignInService.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/services/SignInService.kt
@@ -2,10 +2,12 @@ package com.msg.gauth.domain.auth.services
 
 import com.msg.gauth.domain.auth.RefreshToken
 import com.msg.gauth.domain.auth.exception.PasswordMismatchException
+import com.msg.gauth.domain.auth.exception.UserIsPendingException
 import com.msg.gauth.domain.auth.presentation.dto.request.SigninRequestDto
 import com.msg.gauth.domain.auth.presentation.dto.response.SigninResponseDto
 import com.msg.gauth.domain.auth.repository.RefreshTokenRepository
 import com.msg.gauth.domain.user.User
+import com.msg.gauth.domain.user.enums.UserState
 import com.msg.gauth.domain.user.exception.UserNotFoundException
 import com.msg.gauth.domain.user.repository.UserRepository
 import com.msg.gauth.global.security.jwt.JwtTokenProvider
@@ -26,7 +28,8 @@ class SignInService(
         val user: User = userRepository.findByEmail(dto.email) ?: throw UserNotFoundException()
         if (!passwordEncoder.matches(dto.password, user.password))
             throw PasswordMismatchException()
-
+        if(user.state != UserState.CREATED)
+            throw UserIsPendingException()
         val access = jwtTokenProvider.generateAccessToken(dto.email)
         val refresh = jwtTokenProvider.generateRefreshToken(dto.email)
         val expiresAt = jwtTokenProvider.accessExpiredTime
