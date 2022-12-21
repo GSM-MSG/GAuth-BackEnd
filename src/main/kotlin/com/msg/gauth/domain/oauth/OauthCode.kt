da5ce942271a94aefe19574af9a495e0a11d463b
jeayoung <k01066624566@gmail.com>
2022-12-21 09:21:09 +0900

    OauthCode의 timeToLive가 잘못 설정되있길래 수정했습니다

--- src/main/kotlin/com/msg/gauth/domain/oauth/OauthCode.kt
+++ src/main/kotlin/com/msg/gauth/domain/oauth/OauthCode.kt
@@ -3,7 +3,7 @@ package com.msg.gauth.domain.oauth
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
-@RedisHash(value = "oauthCode", timeToLive = 1000L * 60 * 5)
+@RedisHash(value = "oauthCode", timeToLive = 60 * 5)
 class OauthCode(
     @Id
     val code: String,
