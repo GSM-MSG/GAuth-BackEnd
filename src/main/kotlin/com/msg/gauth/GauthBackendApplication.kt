d1b8e299f08d1e8ec4e53f2fa5b3f4237586dfa6
jeayoung <k01066624566@gmail.com>
2022-12-29 20:02:15 +0900

    ZonedDateTime 서울로 고정

--- src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
+++ src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
@@ -3,11 +3,13 @@ package com.msg.gauth
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class GauthBackendApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<GauthBackendApplication>(*args)
 }
