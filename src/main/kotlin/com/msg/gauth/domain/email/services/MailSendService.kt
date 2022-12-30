210206dbfa1a426f5af94a520a204a44bc859f6e
jeayoung <k01066624566@gmail.com>
2022-12-30 12:21:45 +0900

    메일 subject Gauth -> GAuth로 수정

--- src/main/kotlin/com/msg/gauth/domain/email/services/MailSendService.kt
+++ src/main/kotlin/com/msg/gauth/domain/email/services/MailSendService.kt
@@ -46,7 +46,7 @@ class MailSendService(
             val msg =
                 "<a href=\"https://server.gauth.co.kr/email/authentication?email=$email&uuid=$value\" style=\"padding: 10px; border: none; color: white; background-color: skyblue; border-radius: 8px; align-self: center; text-align: center;\">인증하기</a>"
             message.addRecipients(Message.RecipientType.TO, emailSendDto.email)
-            message.subject = "[Gauth] 이메일 인증"
+            message.subject = "[GAuth] 이메일 인증"
             message.setText(msg, "utf-8", "html")
             mailSender.send(message)
         } catch (ex: MessagingException) {
