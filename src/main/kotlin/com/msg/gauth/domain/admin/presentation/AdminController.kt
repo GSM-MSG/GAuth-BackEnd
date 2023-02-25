8063dc1fa4c4de85c836defdfb595bb45d18e738
aesperer <piyrw9754@gmail.com>
2023-02-25 15:48:53 +0900

    :sparkles: 이름으로 서비스 검색 api
    api

--- src/main/kotlin/com/msg/gauth/domain/admin/presentation/AdminController.kt
+++ src/main/kotlin/com/msg/gauth/domain/admin/presentation/AdminController.kt
@@ -1,18 +1,26 @@
 package com.msg.gauth.domain.admin.presentation
 
+import com.msg.gauth.domain.admin.presentation.dto.ClientResDto
 import com.msg.gauth.domain.admin.services.ExcelParsingService
+import com.msg.gauth.domain.admin.services.GetClientsByServiceNameService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/admin")
 class AdminController(
-    val excelParsingService: ExcelParsingService
+    private val excelParsingService: ExcelParsingService,
+    private val getClientsByServiceNameService: GetClientsByServiceNameService
 ) {
+    @GetMapping
+    fun getClientsByServiceName(pageable: Pageable,@RequestParam serviceName: String): ResponseEntity<Page<ClientResDto>> {
+        val result = getClientsByServiceNameService.execute(pageable, serviceName)
+        return ResponseEntity.ok(result)
+    }
+
     @PostMapping("/parsing-member")
     fun parseMember(@RequestParam("studentInfo") file: MultipartFile): ResponseEntity<Void>{
         excelParsingService.execute(file)
