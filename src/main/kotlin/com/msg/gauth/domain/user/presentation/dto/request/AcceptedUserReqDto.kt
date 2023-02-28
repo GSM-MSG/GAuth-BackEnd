e8e762c3067d165e7320d00c11068e12583f9772
KimTaeO <taeo0328@gmail.com>
2023-02-28 15:25:19 +0900

    :sparkles: 수락 유저 리스트 RequestDto 작성

---
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptedUserReqDto.kt
@@ -0,0 +1,7 @@
+package com.msg.gauth.domain.user.presentation.dto.request
+
+class AcceptedUserReqDto(
+    val grade: Int,
+    val classNum: Int,
+    val keyword: String
+)
---
+++ src/main/kotlin/com/msg/gauth/domain/user/specification/UserSpecification.kt
@@ -0,0 +1,4 @@
+package com.msg.gauth.domain.user.specification
+
+class UserSpecification {
+}
