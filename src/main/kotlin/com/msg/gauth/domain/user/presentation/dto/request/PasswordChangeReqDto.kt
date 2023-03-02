a0a2343407b83b6592d2621b0c6bfe8abab982a7
조재영 <80161826+dolong2@users.noreply.github.com>
2023-03-02 14:56:12 +0900

    Update src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/PasswordChangeReqDto.kt
    Co-authored-by: baegteun <baegteun@gmail.com>

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/PasswordChangeReqDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/PasswordChangeReqDto.kt
@@ -3,7 +3,7 @@ package com.msg.gauth.domain.user.presentation.dto.request
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Pattern
 
-data class  PasswordChangeReqDto(
+data class PasswordChangeReqDto(
     @field:NotBlank
     @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#\$%^*+=-?<>])(?=.*[0-9]).{8,20}\$")
     val password: String,
