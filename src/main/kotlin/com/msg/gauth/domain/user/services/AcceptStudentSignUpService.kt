0a9a5cb8455df94f4a9a3fd4a8631c652bdbc8c4
KimTaeO <taeo0328@gmail.com>
2023-03-06 20:47:34 +0900

    :sparkles: 학생 수락 서비스 작성

--- src/main/kotlin/com/msg/gauth/domain/user/services/AcceptStudentSignUpService.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/services/AcceptStudentSignUpService.kt
@@ -1,4 +1,20 @@
 package com.msg.gauth.domain.user.services
 
-class AcceptStudnetSignUpService {
+import com.msg.gauth.domain.user.enums.UserRole
+import com.msg.gauth.domain.user.enums.UserState
+import com.msg.gauth.domain.user.exception.UserNotFoundException
+import com.msg.gauth.domain.user.presentation.dto.request.AcceptStudentReqDto
+import com.msg.gauth.domain.user.repository.UserRepository
+import com.msg.gauth.global.annotation.service.TransactionalService
+
+@TransactionalService
+class AcceptStudentSignUpService(
+    val userRepository: UserRepository
+) {
+    fun execute(acceptedStudentReqDto: AcceptStudentReqDto) {
+        val user = userRepository.findByIdAndStateAndRoles(acceptedStudentReqDto.id, UserState.PENDING, mutableListOf(UserRole.ROLE_STUDENT))
+            ?: throw UserNotFoundException()
+
+        user.update(acceptedStudentReqDto.name, acceptedStudentReqDto.grade, acceptedStudentReqDto.classNum, acceptedStudentReqDto.num,acceptedStudentReqDto.gender)
+    }
 }
