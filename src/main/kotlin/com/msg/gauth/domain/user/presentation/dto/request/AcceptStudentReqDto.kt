9c73c45d53d61682ba700d4dfe1fe14e5dcdb360
KimTaeO <taeo0328@gmail.com>
2023-03-06 18:23:52 +0900

    :sparkles: 학생 회원가입 수락 request Dto 작성

---
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptStudentReqDto.kt
@@ -0,0 +1,12 @@
+package com.msg.gauth.domain.user.presentation.dto.request
+
+import com.msg.gauth.domain.user.enums.Gender
+
+data class AcceptStudentReqDto(
+    val id: Long,
+    val name: String,
+    val gender: Gender,
+    val grade: Int?,
+    val classNum: Int?,
+    val num: Int?
+)
