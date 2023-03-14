f960b91cb0340ad11d820b8d0fbb56556bd23c7c
KimTaeO <taeo0328@gmail.com>
2023-03-15 00:38:07 +0900

    :sparkles: dto에 검증로직 추가

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptStudentReqDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptStudentReqDto.kt
@@ -1,12 +1,19 @@
 package com.msg.gauth.domain.user.presentation.dto.request
 
 import com.msg.gauth.domain.user.enums.Gender
+import javax.validation.constraints.NotBlank
 
 data class AcceptStudentReqDto(
+    @field: NotBlank
     val id: Long,
+    @field: NotBlank
     val name: String,
+    @field: NotBlank
     val gender: Gender,
+    @field: NotBlank
     val grade: Int,
+    @field: NotBlank
     val classNum: Int,
+    @field: NotBlank
     val num: Int
 )
