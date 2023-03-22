6307062ccdfdb5c950192a86952f723737f2087e
KimTaeO <taeo0328@gmail.com>
2023-03-22 18:03:09 +0900

    :sparkles: 리포지토리 변경에 따른 서비스 로직 재구성

--- src/main/kotlin/com/msg/gauth/domain/user/services/AcceptedUserService.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/services/AcceptedUserService.kt
@@ -9,16 +9,10 @@ import com.msg.gauth.global.annotation.service.ReadOnlyService
 class AcceptedUserService(
     private val userRepository: UserRepository,
 ) {
-    fun execute(grade: Int, classNum: Int, keyword: String): List<SingleAcceptedUserResDto> {
-        var userList = userRepository.findAllByState(UserState.CREATED)
-        if(grade != 0)
-            userList = userList.filter { it.grade == grade }
-        if(classNum != 0)
-            userList = userList.filter { it.classNum == classNum }
-        if(keyword != "0")
-            userList = userList.filter { it.name!!.contains(keyword) }
-
-        return userList.map { SingleAcceptedUserResDto(it) }
-
+    fun execute(grade: Int, classNum: Int, name: String): List<SingleAcceptedUserResDto> {
+        var userList = userRepository.findByStateAndGradeAndClassNumAndNameContaining(UserState.CREATED, grade, classNum, name)
+        return userList.map {
+            SingleAcceptedUserResDto(it)
+        }
     }
 }
