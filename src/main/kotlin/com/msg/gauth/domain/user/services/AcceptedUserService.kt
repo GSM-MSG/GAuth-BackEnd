d18afd5abf742a122f9550382d9662b4542a663a
KimTaeO <taeo0328@gmail.com>
2023-03-23 16:18:26 +0900

    :art: 리포지토리에서 service로 코드 이전
    ''

--- src/main/kotlin/com/msg/gauth/domain/user/services/AcceptedUserService.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/services/AcceptedUserService.kt
@@ -1,5 +1,6 @@
 package com.msg.gauth.domain.user.services
 
+import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.enums.UserState
 import com.msg.gauth.domain.user.presentation.dto.response.SingleAcceptedUserResDto
 import com.msg.gauth.domain.user.repository.UserRepository
@@ -10,7 +11,20 @@ class AcceptedUserService(
     private val userRepository: UserRepository,
 ) {
     fun execute(grade: Int, classNum: Int, name: String): List<SingleAcceptedUserResDto> {
-        var userList = userRepository.findByStateAndGradeAndClassNumAndNameContaining(UserState.CREATED, grade, classNum, name)
+        val userList: List<User>
+        = when {
+            grade == 0 && classNum == 0 && name == "" -> userRepository.findAllByState(UserState.CREATED)
+
+            grade == 0 && classNum == 0 -> userRepository.findAllByStateAndNameContaining(UserState.CREATED, name)
+            classNum == 0 && name == "" -> userRepository.findAllByStateAndClassNum(UserState.CREATED, classNum)
+            grade == 0 && name == ""-> userRepository.findAllByStateAndGrade(UserState.CREATED, grade)
+
+            grade == 0 -> userRepository.findAllByStateAndClassNumAndNameContaining(UserState.CREATED, classNum, name)
+            classNum == 0 -> userRepository.findAllByStateAndGradeAndNameContaining(UserState.CREATED, grade, name)
+            name == "" -> userRepository.findAllByStateAndGradeAndClassNum(UserState.CREATED, grade, classNum)
+
+            else -> userRepository.findAllByStateAndGradeAndClassNumAndNameContaining(UserState.CREATED, grade, classNum, name)
+        }
         return userList.map {
             SingleAcceptedUserResDto(it)
         }
