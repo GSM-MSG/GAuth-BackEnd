6a45397fb19d96b4b69e090581f16559b71cd0c2
KimTaeO <taeo0328@gmail.com>
2023-04-04 14:32:04 +0900

    :sparkles: responseDto에 id 추가

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/response/SinglePendingListResDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/response/SinglePendingListResDto.kt
@@ -3,11 +3,13 @@ package com.msg.gauth.domain.user.presentation.dto.response
 import com.msg.gauth.domain.user.User
 
 data class SinglePendingListResDto(
+    val id: Long,
     val email: String,
     val profileUrl: String?
 ) {
     constructor(user: User): this(
+        id = user.id,
         email = user.email,
-         profileUrl = user.profileUrl
+        profileUrl = user.profileUrl
     )
 }
