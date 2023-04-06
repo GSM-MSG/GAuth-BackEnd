ba9f39cbbeb151be27dc43c23e408cda799b0f63
aesperer <piyrw9754@gmail.com>
2023-04-06 18:30:07 +0900

    :art: :: validation 추가

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptUserReqDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptUserReqDto.kt
@@ -2,10 +2,15 @@ package com.msg.gauth.domain.user.presentation.dto.request
 
 import com.msg.gauth.domain.user.enums.Gender
 import com.msg.gauth.domain.user.enums.UserRole
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class AcceptUserReqDto(
+    @field:NotNull
     val userRole: UserRole,
+    @field:NotBlank
     val name: String,
+    @field:NotNull
     val gender: Gender,
     val grade: Int?,
     val classNum: Int?,
