df45738ab8109bd2606275a26b2ae1142df9364f
aesperer <piyrw9754@gmail.com>
2023-04-11 09:26:02 +0900

    :sparkles: :: GRADUATE 추가

--- src/main/kotlin/com/msg/gauth/domain/user/enums/UserRole.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/enums/UserRole.kt
@@ -3,7 +3,7 @@ package com.msg.gauth.domain.user.enums
 import org.springframework.security.core.GrantedAuthority
 
 enum class UserRole : GrantedAuthority {
-    ROLE_STUDENT, ROLE_TEACHER, ROLE_ADMIN;
+    ROLE_STUDENT, ROLE_TEACHER, ROLE_ADMIN, ROLE_GRADUATE;
 
     override fun getAuthority(): String =
         name
