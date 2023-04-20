2945586e99b4080aa299b33fa9eb0c29a761e188
Kim Hope <105429536+esperar@users.noreply.github.com>
2023-04-20 15:34:15 +0900

    Update src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
    Co-authored-by: baegteun <baegteun@gmail.com>

--- src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
+++ src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
@@ -1,6 +1,5 @@
 package com.msg.gauth
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
