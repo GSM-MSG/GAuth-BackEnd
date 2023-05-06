b520f1ec8a481a768ca7cdb670dd4a2d486062e3
KimTaeO <taeo0328@gmail.com>
2023-05-06 17:30:57 +0900

    :bug: 그저 "자바"

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptedUserRequest.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptedUserRequest.kt
@@ -4,8 +4,8 @@ import com.msg.gauth.domain.user.enums.UserRole
 import org.springframework.web.bind.annotation.RequestParam
 
 data class AcceptedUserRequest(
-    @RequestParam(defaultValue = "0") val grade: Int,
-    @RequestParam(defaultValue = "0") val classNum: Int,
-    @RequestParam(defaultValue = "") val keyword: String,
-    @RequestParam(defaultValue = "ROLE_STUDENT") val role: UserRole
+    @RequestParam val grade: Int = 0,
+    @RequestParam val classNum: Int = 0,
+    @RequestParam val keyword: String = "",
+    @RequestParam val role: UserRole = UserRole.ROLE_STUDENT
 )
