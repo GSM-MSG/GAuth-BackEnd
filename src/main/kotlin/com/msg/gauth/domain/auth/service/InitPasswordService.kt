cb7d34f187e3e71ca2fddbbeea6e01aed6c73d5c
aesperer <piyrw9754@gmail.com>
2023-05-27 18:54:44 +0900

    refacor :: base time entity, user 개선

--- src/main/kotlin/com/msg/gauth/domain/auth/service/InitPasswordService.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/service/InitPasswordService.kt
@@ -4,6 +4,7 @@ import com.msg.gauth.domain.email.repository.EmailAuthRepository
 import com.msg.gauth.domain.user.exception.EmailNotVerifiedException
 import com.msg.gauth.domain.user.exception.UserNotFoundException
 import com.msg.gauth.domain.auth.presentation.dto.request.PasswordInitReqDto
+import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.repository.UserRepository
 import com.msg.gauth.global.annotation.service.TransactionalService
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -21,8 +22,24 @@ class InitPasswordService(
             throw EmailNotVerifiedException()
         val user = userRepository.findByEmail(passwordInitReqDto.email)
             ?: throw UserNotFoundException()
-        val update = user.update(passwordEncoder.encode(passwordInitReqDto.newPassword))
-        userRepository.save(update)
+
+        user.let {
+            userRepository.save(
+                User(
+                    id = it.id,
+                    email = it.email,
+                    password = passwordEncoder.encode(passwordInitReqDto.newPassword),
+                    gender = it.gender,
+                    name = it.name,
+                    grade = it.grade,
+                    classNum = it.classNum,
+                    num = it.num,
+                    roles = it.roles,
+                    state = it.state,
+                    profileUrl = it.profileUrl
+                )
+            )
+        }
         emailAuthRepository.delete(emailAuth)
     }
 }
--- src/main/kotlin/com/msg/gauth/domain/user/User.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/User.kt
@@ -3,14 +3,14 @@ package com.msg.gauth.domain.user
 import com.msg.gauth.domain.user.enums.Gender
 import com.msg.gauth.domain.user.enums.UserRole
 import com.msg.gauth.domain.user.enums.UserState
-import com.msg.gauth.domain.user.presentation.dto.request.AcceptStudentReqDto
-import com.msg.gauth.domain.user.presentation.dto.request.AcceptUserReqDto
 import com.msg.gauth.global.entity.BaseIdEntity
 import javax.persistence.*
 import javax.validation.constraints.Size
 
 @Entity
 class User(
+
+    override val id: Long,
 
     @Column(unique = true)
     val email: String,
@@ -43,124 +43,6 @@ class User(
 
     @Column(nullable = true, columnDefinition = "TEXT")
     val profileUrl: String?,
-): BaseIdEntity(){
-
-    fun update(name: String, grade: Int, classNum: Int, num: Int, gender: Gender): User{
-        val user = User(
-            name = name,
-            email = this.email,
-            grade = grade,
-            classNum = classNum,
-            num = num,
-            gender = gender,
-            password = this.password,
-            roles = this.roles,
-            state = UserState.CREATED,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-
-    fun updateTeacher(name: String, gender: Gender): User {
-        val user = User(
-            name = name,
-            email = this.email,
-            grade = this.grade,
-            classNum = this.classNum,
-            num = this.num,
-            gender = gender,
-            password = this.password,
-            roles = mutableListOf(UserRole.ROLE_TEACHER),
-            state = UserState.CREATED,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-
-    fun updateGraduate(name: String, gender: Gender): User {
-        val user = User(
-            name = name,
-            email = this.email,
-            grade = this.grade,
-            classNum = this.classNum,
-            num = this.num,
-            gender = gender,
-            password = this.password,
-            roles = mutableListOf(UserRole.ROLE_GRADUATE),
-            state = UserState.CREATED,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-
-    fun update(password: String): User{
-        val user = User(
-            name = this.name,
-            email = this.email,
-            grade = this.grade,
-            classNum = this.classNum,
-            num = this.num,
-            gender = this.gender,
-            password = password,
-            roles = this.roles,
-            state = this.state,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-    fun update(acceptedStudentReqDto: AcceptStudentReqDto): User{
-        val user = User(
-            name = acceptedStudentReqDto.name,
-            email = this.email,
-            grade = acceptedStudentReqDto.grade,
-            classNum = acceptedStudentReqDto.classNum,
-            num = acceptedStudentReqDto.num,
-            gender = acceptedStudentReqDto.gender,
-            password = this.password,
-            roles = this.roles,
-            state = UserState.CREATED,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-
-    fun update(acceptUserReqDto: AcceptUserReqDto): User{
-        val user = User(
-            name = acceptUserReqDto.name,
-            email = this.email,
-            grade = acceptUserReqDto.grade,
-            classNum = acceptUserReqDto.classNum,
-            num = acceptUserReqDto.num,
-            gender = acceptUserReqDto.gender,
-            password = this.password,
-            roles = this.roles,
-            state = UserState.CREATED,
-            profileUrl = this.profileUrl
-        )
-        user.id = this.id
-        return user
-    }
-
-    fun updateProfile(profileUrl: String): User{
-        val user = User(
-            name = this.name,
-            email = this.email,
-            grade = this.grade,
-            classNum = this.classNum,
-            num = this.num,
-            gender = this.gender,
-            password = this.password,
-            roles = this.roles,
-            state = this.state,
-            profileUrl = profileUrl
-        )
-        user.id = this.id
-        return user
-    }
+): BaseIdEntity(id){
 
 }
