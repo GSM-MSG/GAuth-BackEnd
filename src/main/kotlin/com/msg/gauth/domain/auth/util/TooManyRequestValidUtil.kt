597ed4596d00af13d9624224d2f711d6b1e3fa57
aesperer <piyrw9754@gmail.com>
2023-05-30 20:39:00 +0900

    refactor :: too many request valid util

--- src/main/kotlin/com/msg/gauth/domain/auth/util/TooManyRequestValidUtil.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/util/TooManyRequestValidUtil.kt
@@ -6,6 +6,7 @@ import com.msg.gauth.domain.auth.exception.SignInMinuteCountOverException
 import com.msg.gauth.domain.auth.exception.SignInSecondCountOverException
 import com.msg.gauth.domain.auth.repository.MinuteSignInCountRepository
 import com.msg.gauth.domain.auth.repository.SecondSignInCountRepository
+import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.enums.UserState
 import com.msg.gauth.domain.user.exception.UserNotFoundException
 import com.msg.gauth.domain.user.repository.UserRepository
@@ -21,10 +22,11 @@ class TooManyRequestValidUtil(
     fun validRequest(email: String){
         val secondSignInCount = (secondSignInCountRepository.findByIdOrNull(email)
             ?: secondSignInCountRepository.save(SecondSignInCount(email)))
+
         if (secondSignInCount.count >= 20) {
             val user = userRepository.findByEmail(email)
                 ?: throw UserNotFoundException()
-            userRepository.save(user.updateUserState(UserState.SIGN_IN_BAN))
+            userSignInBan(user)
             throw SignInSecondCountOverException()
         }
         secondSignInCount.addCount()
@@ -34,10 +36,30 @@ class TooManyRequestValidUtil(
         if (minuteSignInCount.count >= 10) {
             val user = userRepository.findByEmail(email)
                 ?: throw UserNotFoundException()
-            userRepository.save(user.updateUserState(UserState.SIGN_IN_BAN))
+            userSignInBan(user)
             throw SignInMinuteCountOverException()
         }
         minuteSignInCount.addCount()
         minuteSignInCountRepository.save(minuteSignInCount)
+    }
+
+    fun userSignInBan(user: User){
+        userRepository.save(
+            User(
+                id = user.id,
+                email = user.email,
+                password = user.password,
+                gender = user.gender,
+                name = user.name,
+                grade = user.grade,
+                classNum = user.classNum,
+                num = user.num,
+                roles = user.roles,
+                state = UserState.SIGN_IN_BAN,
+                profileUrl = user.profileUrl,
+                wrongPasswordCount = user.wrongPasswordCount,
+                oauthWrongPasswordCount = user.oauthWrongPasswordCount
+            )
+        )
     }
 }
--- src/main/kotlin/com/msg/gauth/domain/oauth/presentation/OauthController.kt
+++ src/main/kotlin/com/msg/gauth/domain/oauth/presentation/OauthController.kt
@@ -19,17 +19,11 @@ class OauthController(
     private val generateOauthCodeService: GenerateOauthCodeService,
     private val generateOauthTokenService: GenerateOauthTokenService,
     private val refreshOauthTokenService: RefreshOauthTokenService,
-    private val getServiceNameService: GetServiceNameService,
+    private val getServiceNameService: GetServiceNameService
 ) {
     @PostMapping("/code")
     fun generateOauthCode(@Valid @RequestBody oauthCodeRequestDto : OauthCodeRequestDto): ResponseEntity<OauthCodeResponseDto> {
         val result = generateOauthCodeService.execute(oauthCodeRequestDto)
-        return ResponseEntity.ok(result)
-    }
-
-    @PostMapping("/code/access")
-    fun generateOauthCode(): ResponseEntity<OauthCodeResponseDto>{
-        val result = generateOauthCodeService.execute()
         return ResponseEntity.ok(result)
     }
 
