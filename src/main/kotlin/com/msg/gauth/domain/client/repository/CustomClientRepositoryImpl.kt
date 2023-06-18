4834abe4c28bdddaaf14d579d30fe0018e3a2bb3
KimTaeO <taeo0328@gmail.com>
2023-06-18 16:12:34 +0900

    :recycle: :: 단일 Expression을 쉼표을 사용해 AND 연산으로 사용

--- src/main/kotlin/com/msg/gauth/domain/client/repository/CustomClientRepositoryImpl.kt
+++ src/main/kotlin/com/msg/gauth/domain/client/repository/CustomClientRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.msg.gauth.domain.client.repository
 
 import com.msg.gauth.domain.client.QClient.client
 import com.msg.gauth.domain.user.User
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
@@ -10,11 +11,8 @@ class CustomClientRepositoryImpl(
     private val jpaQueryFactory: JPAQueryFactory
 ): CustomClientRepository {
     override fun deleteAllByIdsAndCreatedBy(ids: List<Long>, createdBy: User) {
-
-        val booleanExpression = (client.id.`in`(ids)).and(client.createdBy.eq(createdBy))
-
         jpaQueryFactory.delete(client)
-            .where(booleanExpression)
+            .where(client.id.`in`(ids), client.createdBy.eq(createdBy))
             .execute()
     }
 }
