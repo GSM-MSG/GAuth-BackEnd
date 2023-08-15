4ac8da46fb7f0f4990f8d96f17b26edd8940bcbe
KimTaeO <taeo0328@gmail.com>
2023-08-16 05:18:56 +0900

    :beers: :: 실수로메인브랜치에커밋해버렸습니다죄송합니다죽을죄를지었습니다

--- src/main/kotlin/com/msg/gauth/domain/user/repository/CustomUserRepositoryImpl.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/repository/CustomUserRepositoryImpl.kt
@@ -1,10 +1,15 @@
 package com.msg.gauth.domain.user.repository
 
+import com.fasterxml.jackson.databind.util.ArrayBuilders
 import com.msg.gauth.domain.user.QUser.user
 import com.msg.gauth.domain.user.User
-import com.msg.gauth.domain.user.enums.UserRole
+import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 @Repository
 class CustomUserRepositoryImpl(
@@ -12,13 +17,19 @@ class CustomUserRepositoryImpl(
 ) : CustomUserRepository {
 
     override fun search(grade: Int, classNum: Int, keyword: String): List<User> {
+
         val expression = (if (grade != 0) user.grade.eq(grade) else null)
             ?.and(if (classNum != 0) user.classNum.eq(classNum) else null)
             ?.and(if (keyword.isNotEmpty()) user.name.like("%$keyword%") else null)
-            ?: user.roles.contains(UserRole.ROLE_STUDENT)
 
-        return jpaQueryFactory.selectFrom(user)
-                    .where(expression)
-                    .fetch()
+        return if(expression == null){
+            jpaQueryFactory.selectFrom(user)
+                .fetch()
+        } else {
+            jpaQueryFactory.selectFrom(user)
+                .where(expression)
+                .fetch()
+        }
     }
+
 }
