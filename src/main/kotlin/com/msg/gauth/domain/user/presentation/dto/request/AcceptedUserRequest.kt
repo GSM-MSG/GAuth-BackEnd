53c2a58ca4a2ba97a1a03844c6aa68ae3d4d5b86
KimTaeO <taeo0328@gmail.com>
2023-08-16 01:41:17 +0900

    :art: :: requestParam null 허용

--- src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptedUserRequest.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/presentation/dto/request/AcceptedUserRequest.kt
@@ -4,8 +4,8 @@ import com.msg.gauth.domain.user.enums.UserRole
 import org.springframework.web.bind.annotation.RequestParam
 
 data class AcceptedUserRequest(
-    @RequestParam val grade: Int = 0,
-    @RequestParam val classNum: Int = 0,
-    @RequestParam val keyword: String = "",
-    @RequestParam val role: UserRole = UserRole.ROLE_STUDENT
+    @RequestParam(required = false) val grade: Int = 0,
+    @RequestParam(required = false) val classNum: Int = 0,
+    @RequestParam(required = false) val keyword: String = "",
+    @RequestParam(required = false) val role: UserRole = UserRole.ROLE_STUDENT
 )
