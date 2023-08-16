1bc61a89d01bfe0f3fe27a3175288ad62e715e43
KimTaeO <taeo0328@gmail.com>
2023-08-16 13:49:47 +0900

    :art: :: 각각의 Expression들을 함수 단위로 분리

--- src/main/kotlin/com/msg/gauth/domain/user/repository/CustomUserRepositoryImpl.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/repository/CustomUserRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.msg.gauth.domain.user.repository
 import com.msg.gauth.domain.user.QUser.user
 import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.enums.UserRole
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
@@ -13,13 +14,27 @@ class CustomUserRepositoryImpl(
 
     override fun search(grade: Int, classNum: Int, keyword: String): List<User> {
 
-        val expression = (if (grade != 0) user.grade.eq(grade) else null)
-            ?.and(if (classNum != 0) user.classNum.eq(classNum) else null)
-            ?.and(if (keyword.isNotEmpty()) user.name.like("%$keyword%") else null)
-            ?: user.roles.contains(UserRole.ROLE_STUDENT)
-
-        return jpaQueryFactory.selectFrom(user)
-            .where(expression)
-            .fetch()
+        return if(grade == 0 && classNum == 0 && keyword.isEmpty()) {
+            jpaQueryFactory.selectFrom(user)
+                .where(roleContains(UserRole.ROLE_STUDENT))
+                .fetch()
+        } else {
+            jpaQueryFactory.selectFrom(user)
+                .where(gradeEq(grade), classNumEq(classNum), keywordLike(keyword))
+                .fetch()
+        }
     }
+
+    private fun gradeEq(grade: Int): BooleanExpression? =
+        if(grade != 0) user.grade.eq(grade) else null
+
+    private fun classNumEq(classNum: Int): BooleanExpression? =
+        if(classNum != 0) user.classNum.eq(classNum) else null
+
+    private fun keywordLike(keyword: String): BooleanExpression? =
+        if(keyword.isNotEmpty()) user.name.like("%${keyword}%") else null
+
+    private fun roleContains(userRole: UserRole): BooleanExpression =
+        user.roles.contains(userRole)
+
 }
