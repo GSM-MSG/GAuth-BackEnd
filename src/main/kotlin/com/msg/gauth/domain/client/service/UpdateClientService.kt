db3f4727c3843ca7271b68eab17bf20ce1be80c1
KimTaeO <taeo0328@gmail.com>
2023-08-19 14:27:51 +0900

    :sparkles: :: 서비스 이미지 업데이트 시 기존의 이미지를 삭제하는 코드 작성

--- src/main/kotlin/com/msg/gauth/domain/client/service/UpdateClientService.kt
+++ src/main/kotlin/com/msg/gauth/domain/client/service/UpdateClientService.kt
@@ -6,15 +6,20 @@ import com.msg.gauth.domain.client.presentation.dto.request.ClientUpdateReqDto
 import com.msg.gauth.domain.client.repository.ClientRepository
 import com.msg.gauth.domain.user.util.UserUtil
 import com.msg.gauth.global.annotation.service.TransactionalService
+import com.msg.gauth.global.thirdparty.aws.s3.S3Util
 
 @TransactionalService
 class UpdateClientService(
     private val clientRepository: ClientRepository,
-    private val userUtil: UserUtil
+    private val userUtil: UserUtil,
+    private val s3Util: S3Util
 ) {
     fun updateClient(id: Long, clientUpdateReqDto: ClientUpdateReqDto){
         val client = clientRepository.findByIdAndCreatedBy(id, userUtil.fetchCurrentUser())
             ?: throw ClientNotFindException()
+
+        if(client.serviceImgUrl != "")
+            s3Util.deleteImage(client.serviceImgUrl)
 
         clientRepository.save(clientUpdateReqDto.toEntity(client))
     }
