bd37fb8e1cdf02b1031fdd6ac11c984fea8439c8
KimTaeO <taeo0328@gmail.com>
2023-08-30 20:14:25 +0900

    :art: :: 요청 방식 변경에 따른 requestDTO 변경

--- src/main/kotlin/com/msg/gauth/domain/client/presentation/dto/request/ClientUpdateReqDto.kt
+++ src/main/kotlin/com/msg/gauth/domain/client/presentation/dto/request/ClientUpdateReqDto.kt
@@ -2,8 +2,10 @@ package com.msg.gauth.domain.client.presentation.dto.request
 
 import com.msg.gauth.domain.client.Client
 import com.msg.gauth.domain.client.enums.ServiceScope
+import reactor.util.annotation.Nullable
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class ClientUpdateReqDto(
@@ -17,10 +19,13 @@ data class ClientUpdateReqDto(
     val redirectUri: String,
 
     @Enumerated(EnumType.STRING)
-    val serviceScope: ServiceScope = ServiceScope.PUBLIC
+    val serviceScope: ServiceScope = ServiceScope.PUBLIC,
+
+    @field:NotNull
+    val serviceImgUrl: String
 ) {
 
-    fun toEntity(client: Client, serviceImgUrl: String): Client =
+    fun toEntity(client: Client): Client =
         Client(
             id = client.id,
             clientId = client.clientId,
