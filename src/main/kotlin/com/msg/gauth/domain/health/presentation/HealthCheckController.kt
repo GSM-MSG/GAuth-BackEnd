cb1196ad5398ac6e86b7465001126c215079abfc
Umjiseung <eom071005123@gmail.com>
2023-12-13 14:35:02 +0900

    add::healthCheck api 추가

---
+++ src/main/kotlin/com/msg/gauth/domain/health/presentation/HealthCheckController.kt
@@ -0,0 +1,17 @@
+package com.msg.gauth.domain.health.presentation
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthCheckController {
+
+    @GetMapping
+    fun healthCheck(): ResponseEntity<String> {
+        return ResponseEntity.ok("Application is alive!")
+    }
+
+}
