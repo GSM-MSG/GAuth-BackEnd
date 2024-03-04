bafb9813f2ef11d6885906c32f906e19f9f3ca21
KimTaeO <taeo0328@gmail.com>
2024-03-05 08:36:28 +0900

    add :: user role find query

--- src/main/kotlin/com/msg/gauth/domain/user/repository/UserRoleRepository.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/repository/UserRoleRepository.kt
@@ -1,7 +1,9 @@
 package com.msg.gauth.domain.user.repository
 
+import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.UserRole
 import org.springframework.data.repository.CrudRepository
 
-interface UserRoleRepository:  CrudRepository<UserRole, Long>{
+interface UserRoleRepository:  CrudRepository<UserRole, Long> {
+    fun findByUser(user: User): List<UserRole>
 }
