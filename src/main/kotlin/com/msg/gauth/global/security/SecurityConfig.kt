c620f8761a6770b5086a35e4907a0e952705994c
KimTaeO <taeo0328@gmail.com>
2024-03-05 11:11:48 +0900

    add :: uri GET /user/role

--- src/main/kotlin/com/msg/gauth/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/msg/gauth/global/security/SecurityConfig.kt
@@ -80,6 +80,7 @@ class SecurityConfig(
             .antMatchers(HttpMethod.GET, "/health").permitAll()
 
             // User
+            .antMatchers(HttpMethod.GET, "/user/role").authenticated()
             .antMatchers(HttpMethod.PATCH, "/user/accept-teacher").hasRole("ADMIN")
             .antMatchers(HttpMethod.GET, "/user/pending").hasRole("ADMIN")
             .antMatchers(HttpMethod.PATCH, "/user/accept-student").hasRole("ADMIN")
