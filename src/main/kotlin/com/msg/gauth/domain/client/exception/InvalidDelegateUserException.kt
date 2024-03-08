fab0e4788a06bfc0d08180eab5700817705d5bc8
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-08 14:37:20 +0900

    update :: ErrorCode 수정

--- src/main/kotlin/com/msg/gauth/domain/client/exception/InvalidDelegateUserException.kt
+++ src/main/kotlin/com/msg/gauth/domain/client/exception/InvalidDelegateUserException.kt
@@ -3,4 +3,4 @@ package com.msg.gauth.domain.client.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class NotValidDelegateUserException: BasicException(ErrorCode.BAD_REQUEST)
+class InvalidDelegateUserException: BasicException(ErrorCode.INVALID_DELEGATE_USER)
