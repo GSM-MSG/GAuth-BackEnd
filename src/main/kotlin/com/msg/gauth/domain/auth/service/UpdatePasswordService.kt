6067baaa068fdd533b98e2bae5c724a75ca4b89d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-03-13 23:10:45 +0900

    update :: password 수정 로직 수정

--- src/main/kotlin/com/msg/gauth/domain/auth/service/UpdatePasswordService.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/service/UpdatePasswordService.kt
@@ -2,6 +2,7 @@ package com.msg.gauth.domain.auth.service
 
 import com.msg.gauth.domain.auth.exception.PasswordMismatchException
 import com.msg.gauth.domain.auth.presentation.dto.request.PasswordUpdateRequestDto
+import com.msg.gauth.domain.user.User
 import com.msg.gauth.domain.user.repository.UserRepository
 import com.msg.gauth.domain.user.util.UserUtil
 import com.msg.gauth.global.annotation.service.TransactionalService
@@ -21,6 +22,22 @@ class UpdatePasswordService(
             throw PasswordMismatchException()
         }
 
-        userRepository.save(passwordUpdateRequestDto.toEntity(currentUser, passwordEncoder.encode(passwordUpdateRequestDto.newPassword)))
+        val newPassword = passwordEncoder.encode(passwordUpdateRequestDto.newPassword)
+
+        val user = User(
+            id = currentUser.id,
+            email = currentUser.email,
+            password = newPassword,
+            gender = currentUser.gender,
+            name = currentUser.name,
+            grade = currentUser.grade,
+            classNum = currentUser.classNum,
+            num = currentUser.num,
+            roles = currentUser.roles,
+            state = currentUser.state,
+            profileUrl = currentUser.profileUrl
+        )
+
+        userRepository.save(user)
     }
 }
