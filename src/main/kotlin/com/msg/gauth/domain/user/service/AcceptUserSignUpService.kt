253f99a67ca8f6ac5f5d68e4d846a881dd1afbae
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-04-30 19:44:11 +0900

    add :: saveUserRole 추가

--- src/main/kotlin/com/msg/gauth/domain/user/service/AcceptUserSignUpService.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/service/AcceptUserSignUpService.kt
@@ -1,16 +1,19 @@
 package com.msg.gauth.domain.user.service
 
 import com.msg.gauth.domain.client.exception.BadUserRoleRequestException
+import com.msg.gauth.domain.user.User
+import com.msg.gauth.domain.user.UserRole
 import com.msg.gauth.domain.user.enums.UserRoleType
 import com.msg.gauth.domain.user.enums.UserState
 import com.msg.gauth.domain.user.exception.UserNotFoundException
 import com.msg.gauth.domain.user.presentation.dto.request.AcceptUserReqDto
 import com.msg.gauth.domain.user.repository.UserRepository
+import com.msg.gauth.domain.user.repository.UserRoleRepository
 import com.msg.gauth.global.annotation.service.TransactionalService
-
 @TransactionalService
 class AcceptUserSignUpService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val userRoleRepository: UserRoleRepository
 ) {
 
     fun execute(id: Long, acceptUserReqDto: AcceptUserReqDto) =
@@ -25,13 +28,32 @@ class AcceptUserSignUpService(
         userRepository.findByIdAndState(id, UserState.PENDING)
             ?: throw UserNotFoundException()
 
-    private fun acceptStudent(id: Long, acceptUserReqDto: AcceptUserReqDto)=
-        userRepository.save(acceptUserReqDto.toStudentEntity(getUser(id)))
+    private fun acceptStudent(id: Long, acceptUserReqDto: AcceptUserReqDto) {
+        val user = acceptUserReqDto.toStudentEntity(getUser(id))
+        saveUserRole(user, acceptUserReqDto.userRoleType)
+        userRepository.save(user)
+    }
 
-    private fun acceptTeacher(id: Long, acceptUserReqDto: AcceptUserReqDto) =
-        userRepository.save(acceptUserReqDto.toTeacherEntity(getUser(id)))
+    private fun acceptTeacher(id: Long, acceptUserReqDto: AcceptUserReqDto) {
+        val user = acceptUserReqDto.toTeacherEntity(getUser(id))
+        saveUserRole(user, acceptUserReqDto.userRoleType)
+        userRepository.save(user)
+    }
 
-    private fun acceptGraduate(id: Long, acceptUserReqDto: AcceptUserReqDto) =
-        userRepository.save(acceptUserReqDto.toGraduateEntity(getUser(id)))
+    private fun acceptGraduate(id: Long, acceptUserReqDto: AcceptUserReqDto) {
+        val user = acceptUserReqDto.toGraduateEntity(getUser(id))
+        saveUserRole(user, acceptUserReqDto.userRoleType)
+        userRepository.save(user)
+    }
+
+    private fun saveUserRole(user: User, userRoleType: UserRoleType) {
+        val userRole = UserRole(
+            user = user,
+            userRoleType = userRoleType
+        )
+        userRoleRepository.save(userRole)
+    }
+
 
 }
+
