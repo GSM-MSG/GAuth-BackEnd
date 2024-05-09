17b386fb53c75f30cb4f301e2087f2d6c493f18b
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-09 14:01:27 +0900

    delete :: 쓰이지 않는 메서드 삭제

--- src/main/kotlin/com/msg/gauth/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/repository/UserRepository.kt
@@ -1,24 +1,15 @@
 package com.msg.gauth.domain.user.repository
 
 import com.msg.gauth.domain.user.User
-import com.msg.gauth.domain.user.enums.UserRoleType
 import com.msg.gauth.domain.user.enums.UserState
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface UserRepository: JpaRepository<User, Long>, CustomUserRepository {
     @EntityGraph(attributePaths = ["userRole"])
     fun findByEmail(email: String): User?
     fun findByEmailIn(emailList: List<String>): List<User>
     fun existsByEmail(email: String): Boolean
-    @Query("select user.email from User user")
-    fun findAllEmail(): List<String>
     fun findAllByState(state: UserState): List<User>
-    fun findAllByState(state: UserState, pageable: Pageable): Page<User>
     fun findByIdAndState(id: Long, roles: UserState): User?
-    @Query("select user from User user where user.id = :id and user.state = :state and user.userRole = :userRole")
-    fun findByIdAndStateAndUserRole(id: Long, state: UserState, userRole: UserRoleType): User?
 }
