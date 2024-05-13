2736f0e755b0f308ffd28779b8198716e58ab5eb
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-05-13 10:43:55 +0900

    add :: NotFoundCoworkerException 추가

---
+++ src/main/kotlin/com/msg/gauth/domain/client/exception/NotFoundCoworkerException.kt
@@ -0,0 +1,6 @@
+package com.msg.gauth.domain.client.exception
+
+import com.msg.gauth.global.exception.ErrorCode
+import com.msg.gauth.global.exception.exceptions.BasicException
+
+class NotFoundCoworkerException: BasicException(ErrorCode.NOT_FOUND_COWORKER)
