bd4f7cd94ec1d9f190c8656e87805060837ab33b
KimTaeO <taeo0328@gmail.com>
2024-06-28 16:15:13 +0900

    chore :: email exception 컨벤션 맞춤

--- src/main/kotlin/com/msg/gauth/domain/email/exception/AuthCodeExpiredException.kt
+++ src/main/kotlin/com/msg/gauth/domain/email/exception/AuthCodeExpiredException.kt
@@ -3,4 +3,6 @@ package com.msg.gauth.domain.email.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class AuthCodeExpiredException: BasicException(ErrorCode.AUTH_CODE_EXPIRED)
+class AuthCodeExpiredException : BasicException(
+    ErrorCode.AUTH_CODE_EXPIRED
+)
--- src/main/kotlin/com/msg/gauth/domain/email/exception/AuthCodeNotVerificationException.kt
+++ src/main/kotlin/com/msg/gauth/domain/email/exception/AuthCodeNotVerificationException.kt
@@ -3,4 +3,6 @@ package com.msg.gauth.domain.email.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class AuthCodeNotVerificationException: BasicException(ErrorCode.EMAIL_NOT_VERIFIED)
+class AuthCodeNotVerificationException : BasicException(
+    ErrorCode.EMAIL_NOT_VERIFIED
+)
