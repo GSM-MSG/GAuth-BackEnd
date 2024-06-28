f373ae014643ac326b37a26e9ba59ab5429886c3
KimTaeO <taeo0328@gmail.com>
2024-06-28 17:25:00 +0900

    chore :: 코틀린스럽게

--- src/main/kotlin/com/msg/gauth/domain/email/service/VerifyMailService.kt
+++ src/main/kotlin/com/msg/gauth/domain/email/service/VerifyMailService.kt
@@ -3,6 +3,7 @@ package com.msg.gauth.domain.email.service
 import com.msg.gauth.domain.email.exception.AuthCodeExpiredException
 import com.msg.gauth.domain.email.repository.EmailAuthRepository
 import com.msg.gauth.global.annotation.service.TransactionalService
+import org.springframework.data.repository.findByIdOrNull
 
 @TransactionalService
 class VerifyMailService(
@@ -10,8 +11,8 @@ class VerifyMailService(
 ) {
 
     fun execute(email: String, uuid: String) {
-        val emailAuth = emailAuthRepository.findById(email)
-            .orElseThrow { AuthCodeExpiredException() }
+        val emailAuth = emailAuthRepository.findByIdOrNull(email)
+            ?: throw AuthCodeExpiredException()
 
         if (emailAuth.randomValue != uuid)
             throw AuthCodeExpiredException()
