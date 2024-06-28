fcf161cf61d90c4e26443f887a3c06594837d05e
KimTaeO <taeo0328@gmail.com>
2024-06-28 18:41:40 +0900

    chore :: oauth exception convention

--- src/main/kotlin/com/msg/gauth/domain/oauth/exception/ClientSecretMismatchException.kt
+++ src/main/kotlin/com/msg/gauth/domain/oauth/exception/ClientSecretMismatchException.kt
@@ -3,4 +3,6 @@ package com.msg.gauth.domain.oauth.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class ClientSecretMismatchException: BasicException(ErrorCode.CLIENT_SECRET_MISMATCH)
+class ClientSecretMismatchException : BasicException(
+    ErrorCode.CLIENT_SECRET_MISMATCH
+)
