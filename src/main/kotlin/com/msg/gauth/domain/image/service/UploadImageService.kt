b5c1401cbcb31ce918e2ecc4cdeb9b60de4971db
KimTaeO <taeo0328@gmail.com>
2024-06-28 18:40:05 +0900

    chore :: image service 컨벤션 맞춤

--- src/main/kotlin/com/msg/gauth/domain/image/service/UploadImageService.kt
+++ src/main/kotlin/com/msg/gauth/domain/image/service/UploadImageService.kt
@@ -1,6 +1,5 @@
 package com.msg.gauth.domain.image.service
 
-import com.msg.gauth.domain.admin.exception.FileExtensionInvalidException
 import com.msg.gauth.domain.image.presentation.dto.response.UploadImageResDto
 import com.msg.gauth.global.thirdparty.aws.s3.S3Util
 import org.springframework.stereotype.Service
@@ -10,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile
 class UploadImageService(
     private val s3Util: S3Util
 ) {
+
     fun execute(image: MultipartFile): UploadImageResDto {
         val imgURL = s3Util.imageUpload(image)
 
