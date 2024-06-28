acb475756898853d5d19f3e359bf8769c014135d
KimTaeO <taeo0328@gmail.com>
2024-06-28 15:22:34 +0900

    chore :: auth exception 컨벤션

--- src/main/kotlin/com/msg/gauth/domain/auth/exception/AlreadyExistUserException.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/exception/AlreadyExistUserException.kt
@@ -3,4 +3,6 @@ package com.msg.gauth.domain.auth.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class AlreadyExistUserException : BasicException(ErrorCode.ALREADY_EXIST_USER)
+class AlreadyExistUserException : BasicException(
+    ErrorCode.ALREADY_EXIST_USER
+)
