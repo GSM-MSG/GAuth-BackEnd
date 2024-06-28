f20bc8fac7a86ce08058e556d0f6516879a05f93
KimTaeO <taeo0328@gmail.com>
2024-06-28 14:49:44 +0900

    chore :: auth repository 줄바꿈 정리

--- src/main/kotlin/com/msg/gauth/domain/auth/repository/SecondSignInCountRepository.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/repository/SecondSignInCountRepository.kt
@@ -3,5 +3,4 @@ package com.msg.gauth.domain.auth.repository
 import com.msg.gauth.global.util.count.auth.SecondSignInCount
 import org.springframework.data.repository.CrudRepository
 
-interface SecondSignInCountRepository : CrudRepository<SecondSignInCount, String> {
-}
+interface SecondSignInCountRepository : CrudRepository<SecondSignInCount, String>
--- src/main/kotlin/com/msg/gauth/domain/auth/repository/TempSignInBanRepository.kt
+++ src/main/kotlin/com/msg/gauth/domain/auth/repository/TempSignInBanRepository.kt
@@ -3,5 +3,4 @@ package com.msg.gauth.domain.auth.repository
 import com.msg.gauth.global.util.count.auth.TempSignInBan
 import org.springframework.data.repository.CrudRepository
 
-interface TempSignInBanRepository : CrudRepository<TempSignInBan, String> {
-}
+interface TempSignInBanRepository : CrudRepository<TempSignInBan, String>
