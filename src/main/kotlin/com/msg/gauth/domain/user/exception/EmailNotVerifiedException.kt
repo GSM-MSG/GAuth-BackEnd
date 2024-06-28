80f9f7aadd92a402192248d1281d96790df4b1a2
KimTaeO <taeo0328@gmail.com>
2024-06-28 20:20:21 +0900

    chore :: user exception 컨벤션 맞춤

--- src/main/kotlin/com/msg/gauth/domain/user/exception/EmailNotVerifiedException.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/exception/EmailNotVerifiedException.kt
@@ -3,4 +3,6 @@ package com.msg.gauth.domain.user.exception
 import com.msg.gauth.global.exception.ErrorCode
 import com.msg.gauth.global.exception.exceptions.BasicException
 
-class EmailNotVerifiedException: BasicException(ErrorCode.EMAIL_NOT_VERIFIED)
+class EmailNotVerifiedException : BasicException(
+    ErrorCode.EMAIL_NOT_VERIFIED
+)
