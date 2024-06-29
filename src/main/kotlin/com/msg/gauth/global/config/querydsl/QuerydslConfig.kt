9af022890c5850d2f952554331f9725e4c85d705
KimTaeO <taeo0328@gmail.com>
2024-06-29 14:21:55 +0900

    chore :: querydsl configuration code convention

--- src/main/kotlin/com/msg/gauth/global/config/querydsl/QuerydslConfig.kt
+++ src/main/kotlin/com/msg/gauth/global/config/querydsl/QuerydslConfig.kt
@@ -13,5 +13,6 @@ class QuerydslConfig(
 ) {
 
     @Bean
-    fun JPAQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
+    fun JPAQueryFactory(): JPAQueryFactory =
+        JPAQueryFactory(entityManager)
 }
