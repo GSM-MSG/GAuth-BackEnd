8299f142b40d561a87a782bc885223e2aa71abfb
KimTaeO <taeo0328@gmail.com>
2024-06-29 14:24:20 +0900

    chore :: global exceptions code convention

--- src/main/kotlin/com/msg/gauth/global/exception/exceptions/BadRequestException.kt
+++ src/main/kotlin/com/msg/gauth/global/exception/exceptions/BadRequestException.kt
@@ -2,4 +2,6 @@ package com.msg.gauth.global.exception.exceptions
 
 import com.msg.gauth.global.exception.ErrorCode
 
-class BadRequestException : BasicException(ErrorCode.BAD_REQUEST)
+class BadRequestException : BasicException(
+    ErrorCode.BAD_REQUEST
+)
