f0cfd3fcce379cd7543615308db2500416c83f54
KimTaeO <taeo0328@gmail.com>
2024-06-29 14:31:28 +0900

    chore :: basic exception code convention

--- src/main/kotlin/com/msg/gauth/global/exception/exceptions/BasicException.kt
+++ src/main/kotlin/com/msg/gauth/global/exception/exceptions/BasicException.kt
@@ -2,6 +2,8 @@ package com.msg.gauth.global.exception.exceptions
 
 import com.msg.gauth.global.exception.ErrorCode
 
-open class BasicException(val errorCode: ErrorCode) : RuntimeException(
+open class BasicException(
+    val errorCode: ErrorCode
+) : RuntimeException(
     errorCode.msg
 )
