447b116a527d33d770ef1b7bc1789754a5af4f71
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-01 23:28:01 +0900

    add :: EnableCaching 추가

--- src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
+++ src/main/kotlin/com/msg/gauth/GauthBackendApplication.kt
@@ -3,10 +3,12 @@ package com.msg.gauth
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import java.util.*
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableCaching
 class GauthBackendApplication
 
 fun main(args: Array<String>) {
