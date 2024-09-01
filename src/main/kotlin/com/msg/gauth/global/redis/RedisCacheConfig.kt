bd4efc8f320a4b42066a82153c759478314fe872
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-01 23:26:25 +0900

    add :: RedisCacheConfig 추가

---
+++ src/main/kotlin/com/msg/gauth/global/redis/RedisCacheConfig.kt
@@ -0,0 +1,37 @@
+package com.msg.gauth.global.redis
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+@EnableCaching
+class RedisCacheConfig {
+
+    @Bean
+    fun clientCacheManager(factory: RedisConnectionFactory?): CacheManager {
+        val cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer()
+                )
+            )
+            .entryTtl(Duration.ofMinutes(1L))
+
+        return RedisCacheManager
+            .RedisCacheManagerBuilder
+            .fromConnectionFactory(factory!!)
+            .cacheDefaults(cacheConfig)
+            .build()
+    }
+
+}
