69b46a8c34b1a942f8bf602a18317858cb9cfbd2
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-02 00:14:47 +0900

    add :: Primary 추가

--- src/main/kotlin/com/msg/gauth/global/redis/RedisCacheConfig.kt
+++ src/main/kotlin/com/msg/gauth/global/redis/RedisCacheConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -17,6 +18,7 @@ import java.time.Duration
 class RedisCacheConfig {
 
     @Bean
+    @Primary
     fun clientCacheManager(factory: RedisConnectionFactory?): CacheManager {
         val cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
             .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
