a259f59e8f48aeddde70805416d1c6ef97c572db
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-01 23:53:15 +0900

    add :: Cacheable 추가

--- src/main/kotlin/com/msg/gauth/domain/user/service/GetAcceptedUsersService.kt
+++ src/main/kotlin/com/msg/gauth/domain/user/service/GetAcceptedUsersService.kt
@@ -5,11 +5,14 @@ import com.msg.gauth.domain.user.presentation.dto.request.AcceptedUserRequest
 import com.msg.gauth.domain.user.presentation.dto.response.SingleAcceptedUserResDto
 import com.msg.gauth.domain.user.repository.UserRepository
 import com.msg.gauth.global.annotation.service.ReadOnlyService
+import org.springframework.cache.annotation.Cacheable
 
 @ReadOnlyService
 class GetAcceptedUsersService(
     private val userRepository: UserRepository,
 ) {
+
+    @Cacheable(value = ["AcceptedUser"], cacheManager = "userCacheManager")
     fun execute(request: AcceptedUserRequest): List<SingleAcceptedUserResDto> =
         when (request.role) {
             UserRoleType.ROLE_STUDENT -> userRepository.search(request.grade, request.classNum, request.keyword)
