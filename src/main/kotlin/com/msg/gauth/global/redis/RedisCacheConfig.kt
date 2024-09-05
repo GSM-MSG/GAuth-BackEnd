58f624007acc07b83ecb3e80265846906ad263e6
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-05 15:33:05 +0900

    delete :: import 삭제

--- src/main/kotlin/com/msg/gauth/global/redis/RedisCacheConfig.kt
+++ src/main/kotlin/com/msg/gauth/global/redis/RedisCacheConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
